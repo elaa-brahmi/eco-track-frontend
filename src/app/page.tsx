e105d7a37eb8f88f303bbd9a74c4c9a118902a1b
elaa <elaa.brahmii@gmail.com>
2025-11-21 21:13:10 +0100

    test connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
-import LoginPage from "./(routes)/auth/page";
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import ReportForm from "./(routes)/report/page";
 
 export default function Home() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "loading") return; // wait for session to load
+
+    if (!session?.accessToken) {
+      console.log("No access token found, redirecting...");
+      router.replace("/auth");
+    }
+  }, [session, status, router]);
+
+  if (status === "loading") {
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <LoginPage />
+    <div className="flex min-h-screen flex-col items-center justify-center py-6">
+      <ReportForm />
     </div>
   );
 }
