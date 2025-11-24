13d77afa926a0fe4ad8261707b38d9d376fd830e
elaa <elaa.brahmii@gmail.com>
2025-11-24 23:33:04 +0100

    alter bg color in mobile

--- app/(routes)/admin/dashboard/page.tsx
+++ app/(routes)/admin/dashboard/page.tsx
@@ -3,13 +3,15 @@ import StatsTabs from '@/components/admin/StatsTabs';
 import InteractiveMap from '@/components/admin/InteractiveMap';
 import ActiveRoutes from '@/components/admin/ActiveRoutes';
 import BinsTable from '@/components/admin/BinsTable';
+import RequireRole from '@/utils/RequireRole';
 
 
 export default function AdminDashboard() {
 return (
+<RequireRole roles={["admin-role"]}>
 <div className="flex mt-16">
     <Sidebar />
-    <main className="flex-1 p-6 bg-gray-100 min-h-screen">
+    <main className="flex-1 p-6 md:bg-gray-100 min-h-screen">
         <h1 className="text-2xl font-bold">Monitor</h1>
         <p className="text-gray-500 text-sm">Real-time bin tracking and management</p>
         <StatsTabs />
@@ -24,5 +26,6 @@ return (
         </div>
     </main>
 </div>
+</RequireRole>
 );
 }
