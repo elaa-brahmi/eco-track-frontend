1d81d4df9e4c0213795017e761975c7361e096bb
elaa <elaa.brahmii@gmail.com>
2025-11-24 23:34:22 +0100

    add employee functions of endpoints + add attributes to employee type

--- services/employees.ts
+++ services/employees.ts
@@ -1,11 +1,7 @@
-// services/employee.ts
-export interface CreateEmployeeData {
-  name: string;
-  email: string;
-  password: string;
-}
+import { EmployeeData } from "@/types/EmployeeData";
 
-export const createEmployee = async (data: CreateEmployeeData): Promise<any> => {
+
+export const createEmployee = async (data: EmployeeData): Promise<any> => {
   const res = await fetch("/api/proxy/api/employees", {
     method: "POST",
     headers: {
@@ -21,3 +17,29 @@ export const createEmployee = async (data: CreateEmployeeData): Promise<any> => 
 
   return res.json();
 };
+export const getEmployees = async (): Promise<EmployeeData[]> => {
+  const res = await fetch("/api/proxy/api/employees", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (!res.ok) {
+    const error = await res.text();
+    throw new Error(error || "Failed to fetch employees");
+  }
+  return res.json();
+}
+export const deleteEmployee = async (keycloakId: string): Promise<void> => {
+  const res = await fetch(`/api/proxy/api/employees/${keycloakId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (!res.ok) {
+    const error = await res.text();
+    throw new Error(error || "Failed to delete employee");
+  }
+  return;
+}
